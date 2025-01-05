09a4df477f073b6fdeee87abc7a3a93c8057327e
Almila Duru Kavak <34842530+almiladurukavak@users.noreply.github.com>
2025-01-05 22:32:49 +0300

    update schedule time

--- flutter_project/lib/schedule.dart
+++ flutter_project/lib/schedule.dart
@@ -7,8 +7,15 @@ import 'authprovider.dart';
 import 'login.dart';
 import 'sidebar.dart';
 
-class SchedulePage extends StatelessWidget {
+class SchedulePage extends StatefulWidget {
   const SchedulePage({super.key});
+
+  @override
+  _SchedulePageState createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
+  DateTime? selectedDateTime;
 
   @override
   Widget build(BuildContext context) {
@@ -58,8 +65,9 @@ class SchedulePage extends StatelessWidget {
                             apiEndpoint:
                                 'http://13.60.226.247:8080/api/posts/twitter',
                             filterKey: 'status',
-                            filterValues: ['DRAFT', 'SCHEDULED'],
+                            filterValues: ['SCHEDULED'],
                             textKey: 'tweetText',
+                            platform: 'TWITTER',
                           ),
                           _buildPostList(
                             context,
@@ -69,6 +77,7 @@ class SchedulePage extends StatelessWidget {
                             filterKey: 'status',
                             filterValues: ['SCHEDULED'],
                             textKey: 'caption',
+                            platform: 'INSTAGRAM',
                           ),
                         ],
                       ),
@@ -90,6 +99,7 @@ class SchedulePage extends StatelessWidget {
     required String filterKey,
     required List<String> filterValues,
     required String textKey,
+    required String platform,
   }) {
     return FutureBuilder<List<Map<String, dynamic>>>(
       future: fetchFilteredPosts(
@@ -123,7 +133,7 @@ class SchedulePage extends StatelessWidget {
             itemCount: posts.length,
             itemBuilder: (context, index) {
               final post = posts[index];
-              return _buildPostItem(post, textKey);
+              return _buildPostItem(post, textKey, token, platform);
             },
           );
         }
@@ -131,7 +141,8 @@ class SchedulePage extends StatelessWidget {
     );
   }
 
-  Widget _buildPostItem(Map<String, dynamic> post, String textKey) {
+  Widget _buildPostItem(Map<String, dynamic> post, String textKey, String token,
+      String platform) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: Column(
@@ -148,7 +159,8 @@ class SchedulePage extends StatelessWidget {
                   const SizedBox(width: 20),
                   Text(
                     '${textKey == "tweetText" ? "Tweet" : "Caption"}: ${post[textKey] ?? 'No content'}\n'
-                    'Created at: ${_formatCreatedAt(post['createdAt'])}',
+                    'Created at: ${_formatCreatedAt(post['createdAt'])}\n',
+                    //  'Scheduled at: ${_formatScheduledTime(post['scheduledTime'])}', // Show scheduled time
                     style: const TextStyle(fontSize: 16, color: Colors.black54),
                   ),
                 ],
@@ -156,8 +168,13 @@ class SchedulePage extends StatelessWidget {
               Row(
                 children: [
                   TextButton(
-                    onPressed: () {
-                      // Tarih seçme işlemi
+                    onPressed: () async {
+                      DateTime? newDate = await _selectDate(context);
+                      if (newDate != null) {
+                        setState(() {
+                          selectedDateTime = newDate;
+                        });
+                      }
                     },
                     style: TextButton.styleFrom(
                       backgroundColor: Colors.blue,
@@ -166,26 +183,11 @@ class SchedulePage extends StatelessWidget {
                     child: const Text('Select Date'),
                   ),
                   const SizedBox(width: 10),
-                  TextButton(
+                  IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
                     onPressed: () {
-                      // Onaylama işlemi
+                      _deletePost(context, token, post['id'], platform);
                     },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white,
-                    ),
-                    child: const Text('Accept'),
-                  ),
-                  const SizedBox(width: 10),
-                  TextButton(
-                    onPressed: () {
-                      // Reddetme işlemi
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
-                    ),
-                    child: const Text('Decline'),
                   ),
                 ],
               ),
@@ -195,6 +197,19 @@ class SchedulePage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String _formatScheduledTime(dynamic scheduledTime) {
+    if (scheduledTime != null) {
+      try {
+        final date = DateTime.parse(scheduledTime).toLocal();
+        return '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')} '
+            '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+      } catch (e) {
+        return '';
+      }
+    }
+    return '';
   }
 
   String _formatCreatedAt(dynamic createdAt) {
@@ -238,13 +253,14 @@ class SchedulePage extends StatelessWidget {
       if (response.statusCode == 200) {
         final List<dynamic> data = jsonDecode(response.body);
 
-        // Belirli durumlara göre filtreleme
+        // Filter posts by status
         return data
             .where((e) => filterValues.contains(e[filterKey]))
             .map((e) => {
                   'id': e['id'],
                   textKey: e[textKey],
                   'createdAt': e['createdAt'],
+                  'scheduledTime': e['scheduledTime'], // Fetch scheduledTime
                 })
             .toList();
       } else {
@@ -254,6 +270,118 @@ class SchedulePage extends StatelessWidget {
       }
     } catch (e) {
       throw Exception('Error fetching posts: $e');
+    }
+  }
+
+  Future<void> _deletePost(
+    BuildContext context,
+    String token,
+    int postId,
+    String platform,
+  ) async {
+    final url = Uri.parse('http://13.60.226.247:8080/api/schedule')
+        .replace(queryParameters: {
+      'post_id': postId.toString(),
+      'platform': platform,
+    });
+
+    try {
+      final response = await http.delete(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Scheduled post was deleted successfully!')),
+        );
+
+        // Manually trigger the UI update by calling setState
+        setState(() {
+          // Refresh the post list
+        });
+      } else {
+        throw Exception(
+            'Failed to delete post. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
+  }
+
+  Future<DateTime?> _selectDate(BuildContext context) async {
+    final DateTime currentDate = DateTime.now();
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: currentDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (picked != null) {
+      final TimeOfDay? timePicked = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(currentDate),
+      );
+
+      if (timePicked != null) {
+        return DateTime(
+          picked.year,
+          picked.month,
+          picked.day,
+          timePicked.hour,
+          timePicked.minute,
+        );
+      }
+    }
+    return null;
+  }
+
+  Future<void> _updateScheduledPost(
+    String token,
+    int postId,
+    String platform,
+  ) async {
+    if (selectedDateTime == null) {
+      return;
+    }
+
+    final url = Uri.parse('http://13.60.226.247:8080/api/schedule/');
+    final body = jsonEncode({
+      'scheduled_time': selectedDateTime?.toIso8601String(),
+      'platform': platform,
+      'is_active': true,
+      'post_id': '$postId'
+    });
+
+    try {
+      final response = await http.put(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Post updated successfully!')),
+        );
+      } else {
+        throw Exception(
+            'Failed to update post. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
     }
   }
 }
